c2bac9d44b8864e116359ac2bcd6d74d9736bc78
EpicCodeWizard2 <6488651-EpicCodeWizard2@users.noreply.replit.com>
2023-07-09 06:26:29 +0000

    Added html files

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from flask import *
 app = Flask(__name__, template_folder="", static_folder="")
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
+# @cross_origin()
 
 @app.route("/")
-@cross_origin()
 def home():
   return render_template("index.html")
+
+@app.route("/security_camera/")
+def security_camera():
+  return render_template("camera.html")
 
 app.run(host="0.0.0.0")
