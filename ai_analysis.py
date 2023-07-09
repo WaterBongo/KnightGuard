a3f3f82e8b2b7eb8000b0d4e9fb4d11b41770bc6
EpicCodeWizard2 <6488651-EpicCodeWizard2@users.noreply.replit.com>
2023-07-09 08:27:27 +0000

    Pushing AI model

--- ai_analysis.py
+++ ai_analysis.py
@@ -1,8 +1,27 @@
+import torchvision.transforms as T
 from PIL import Image
-import random
-import numpy
+import functools
+import torch
 import io
 
-def analyze(image):
-  # numpy.asarray(Image.open(io.BytesIO(image)))
-  return random.random()
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+LOAD_MODEL_VERSION = "1.0a"
+LOAD_MODEL_FILENAME = "weapon_trained_model-"+LOAD_MODEL_VERSION+".pt"
+LOAD_DIR = './'
+model = torch.load(LOAD_DIR+LOAD_MODEL_FILENAME, map_location=device)
+model.eval()
+
+def predict(image_byte, model):
+    image = Image.open(io.BytesIO(image_byte)).convert("RGB")
+    transform = T.Compose([T.ToTensor()])
+    image = transform(image).to(device)
+    image = image.unsqueeze(0)
+
+    model = model.to(device)
+    model.eval()
+    with torch.no_grad():
+        prediction = model(image)
+    prediction = prediction[0]
+    return round(prediction['scores'][0].item(),3)
+
+analyze = functools.partial(predict, model=model)
